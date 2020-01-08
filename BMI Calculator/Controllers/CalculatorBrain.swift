810ac7a0b2efad7734f5da3d37366d3027653bd0
Joel Thomson <joelparavara@gmail.com>
2020-01-08 20:26:28 +0530

    first commit

--- BMI Calculator/Controllers/CalculatorBrain.swift
+++ BMI Calculator/Controllers/CalculatorBrain.swift
@@ -21,14 +21,16 @@ struct CalculatorBrain {
         let bmiValue = weight / (height * height)
         
         if bmiValue < 18.5 {
-            print("Underweight")
+            bmi = BMI(value: bmiValue, advice: "Eat more pies", color: #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1))
         } else if bmiValue < 24.9 {
-            print("Normal weight")
+            bmi = BMI(value: bmiValue, advice: "Fit as a fiddle", color: #colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1))
         } else {
-            print("Overweight")
+            bmi = BMI(value: bmiValue, advice: "Eat less pies", color: #colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1))
         }
+    }
+    
+    func getAdvice() {
         
-        //bmi = BMI(value: bmiValue, advice: <#T##String#>, color: <#T##UIColor#>)
     }
     
 }
--- BMI Calculator/Controllers/ResultViewController.swift
+++ BMI Calculator/Controllers/ResultViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ResultViewController: UIViewController {
     
     var bmiValue: String?
+    var advice: String?
 
     @IBOutlet weak var bmiLabel: UILabel!
     @IBOutlet weak var adviceLabel: UILabel!
@@ -18,10 +19,13 @@ class ResultViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         bmiLabel.text = bmiValue
+        adviceLabel.text = advice
     }
     
     @IBAction func recalculatePressed(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
     }
+    
+    
     
 }
